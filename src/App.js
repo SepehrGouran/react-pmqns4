0ca065470639f6d0af7bcef5b1287bfb480f0335
SepehrGouran <sepehr.gouran@gmail.com>
2020-08-16 23:49:06 +0430

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./style.css";
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <p>Sepehr</p>
     </div>
   );
 }
